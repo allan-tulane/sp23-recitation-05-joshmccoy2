05cd1af095dd02205b2bba127aeb2ef250166c08
Josh McCoy <112521093+joshmccoy2@users.noreply.github.com>
2023-03-22 01:57:50 +0000

    Final

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
 # CMPS 2200 Reciation 5
 ## Answers
 
-**Name:**_________________________
+**Name:** Josh McCoy
 
 
 Place all written answers from `recitation-06.md` here for easier grading.
@@ -13,8 +13,46 @@ Place all written answers from `recitation-06.md` here for easier grading.
 
 
 - **1b.**
+|      n |   qsort-fixed-pivot |   qsort-random-pivot |
+|--------|---------------------|----------------------|
+|    100 |               0.001 |                0.008 |
+|    200 |               0.000 |                0.002 |
+|    500 |               0.000 |                0.002 |
+|   1000 |               0.001 |                0.003 |
+|   2000 |               0.001 |                0.002 |
+|   5000 |               0.001 |                0.004 |
+|  10000 |               0.001 |                0.004 |
+|  20000 |               0.001 |                0.004 |
+|  50000 |               0.002 |                0.005 |
+| 100000 |               0.002 |                0.008 |
 
+|      n |   ssort-fixed-pivot |   ssort-random-pivot |
+|--------|---------------------|----------------------|
+|    100 |               0.001 |                0.007 |
+|    200 |               0.000 |                0.002 |
+|    500 |               0.000 |                0.002 |
+|   1000 |               0.000 |                0.002 |
+|   2000 |               0.000 |                0.002 |
+|   5000 |               0.002 |                0.006 |
+|  10000 |               0.001 |                0.005 |
+|  20000 |               0.002 |                0.005 |
+|  50000 |               0.002 |                0.005 |
+| 100000 |               0.002 |                0.006 |
 
-
+Ssort has a much higher runtime than that qsort for both sorted and unsorted lists. The asymptotic upper bound of qsort is O(nlogn) and ssort is O(n^2). Ssort did perform better for sorted lists for any size n.
 
 - **1c.**
+n	qsort-fixed-pivot	qsort-random-pivot	tim_sort
+100	0.001	0.004	0.018
+200	0.001	0.003	0.026
+500	0.001	0.003	0.070
+1000	0.001	0.003	0.159
+2000	0.001	0.003	3.218
+5000	0.002	0.005	1.194
+10000	0.003	0.006	3.572
+20000	0.003	0.006	5.888
+50000	0.003	0.006	38.698
+100000	0.002	0.006	33.340
+
+
+The fastest sorting is qsort-fixed-pivot. For any value n the runntime is smaller than tim sort. If the data became somewhat sorted than tim_sort would be the fastest option.  Both have 0(nlogn) worst case runtimes, However mostly Qsort is faster when the list is random. 
--- main.py
+++ main.py
@@ -3,11 +3,37 @@ import tabulate
 
 
 def qsort(a, pivot_fn):
-    ## TO DO
-    pass
-    
+  if len(a) == 0:
+    return a
+  else:
+    p = pivot_fn(a)
+    l = list(filter(lambda x: x < pivot, a))
+    m = list(filter(lambda x: x == pivot, a))
+    r = list(filter(lambda x: x > pivot, a))
+
+    return qsort(l, pivot_fn) + m + qsort(r, pivot_fn)
+
+    return l + [p] + r
+
+
+def ssort(L):
+  for i in range(len(L)):
+    #print(L)
+    m = L.index(min(L[i:]))
+    L[i], L[m] = L[m], L[i]
+  return L
+
+
+def randomize(a):
+  return random.choice(a)
+
+
+def fixed(a):
+  return a[0]
+
+
 def time_search(sort_fn, mylist):
-    """
+  """
     Return the number of milliseconds to run this
     sort function on this list.
 
@@ -24,13 +50,15 @@ def time_search(sort_fn, mylist):
       the number of milliseconds it takes to run this
       search function on this input.
     """
-    start = time.time()
-    sort_fn(mylist)
-    return (time.time() - start) * 1000
-    ###
+  start = time.time()
+  sort_fn(mylist)
+  return (time.time() - start) * 1000
+  ###
 
-def compare_sort(sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]):
-    """
+
+def compare_sort(
+    sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]):
+  """
     Compare the running time of different sorting algorithms.
 
     Returns:
@@ -39,33 +67,38 @@ def compare_sort(sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 10
       indicating the number of milliseconds it takes
       for each method to run on each value of n
     """
-    ### TODO - sorting algorithms for comparison
-    qsort_fixed_pivot = # 
-    qsort_random_pivot = #
-    tim_sort = #
-    result = []
-    for size in sizes:
-        # create list in ascending order
-        mylist = list(range(size))
-        # shuffles list if needed
-        #random.shuffle(mylist)
-        result.append([
-            len(mylist),
-            time_search(qsort_fixed_pivot, mylist),
-            time_search(qsort_random_pivot, mylist),
-        ])
-    return result
-    ###
+  ### TODO - sorting algorithms for comparison
+  qsort_fixed_pivot = lambda a: a[0]
+  qsort_random_pivot = lambda a: random.choice(a)
+  tim_sort = lambda a: sorted(a)
+  selection_sort = lambda a: ssort(a)
+  result = []
+  for size in sizes:
+    # create list in ascending order
+    mylist = list(range(size))
+    # shuffles list if needed
+    #random.shuffle(mylist)
+    result.append([
+      len(mylist),
+      time_search(qsort_fixed_pivot, mylist),
+      time_search(qsort_random_pivot, mylist),
+    ])
+  return result
+  ###
+
 
 def print_results(results):
-    """ change as needed for comparisons """
-    print(tabulate.tabulate(results,
-                            headers=['n', 'qsort-fixed-pivot', 'qsort-random-pivot'],
-                            floatfmt=".3f",
-                            tablefmt="github"))
+  """ change as needed for comparisons """
+  print(
+    tabulate.tabulate(results,
+                      headers=['n', 'ssort-fixed-pivot', 'ssort-random-pivot'],
+                      floatfmt=".3f",
+                      tablefmt="github"))
+
 
 def test_print():
-    print_results(compare_sort())
+  print_results(compare_sort())
+
 
 random.seed()
 test_print()
